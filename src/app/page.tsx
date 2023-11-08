e9e3c77d09378b70b452764ea9be49c9b9b49433
gmccosta <chrishanthacosta@gmail.com>
2023-11-08 12:08:12 +0530

    sss

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,12 @@
 import Image from 'next/image'
 import { Dashboard } from './components/dashboard';
+import Navbar from './components/navbar';
 
 export default function Home() {
   return (
-   <Dashboard/>
+    <>
+     <Navbar></Navbar>
+      <Dashboard />
+    </>
   )
 }
